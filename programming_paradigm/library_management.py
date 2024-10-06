d49ad05458e17c23c3fdd2f67bef1835586e74a7
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-10-06 12:06:01 +0300

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_checked_out = False
+class Library:
+    def __init__(self):
+        self.books = []  
+
+    def add_book(self,title,author):
+        new_book = Book(title, author)
+        self.books.append(new_book)
+
+    def check_out_book(self,title):
+        for book in self.books:
+            if book.title == title and not book.is_checked_out:
+                book.is_checked_out = True
+                return True
+        return False
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title and book.is_checked_out:
+                book.is_checked_out = False
+                return True
+        return False
+    def list_available_books(self):
+        available_books = [f"{book.title} by {book.author}" for book in self.books if not book.is_checked_out]
+        print("\n".join(available_books))
---
+++ programming_paradigm/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+  # # Initial list of available books
+    # print("Available books after setup:")
+    # library.list_available_books()
+
+    # # Simulate checking out a book
+    # library.check_out_book("1984")
+    # print("\nAvailable books after checking out '1984':")
+    # library.list_available_books()
+
+    # # Simulate returning a book
+    # library.return_book("1984")
+    # print("\nAvailable books after returning '1984':")
+    # library.list_available_books()
